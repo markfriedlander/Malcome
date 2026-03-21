601d38b909369b4c5a292ec0707a197c9ffc838b
Mark Friedlander <markfriedlander@yahoo.com>
2026-03-21 10:28:24 -0700

    Initial Commit

---
+++ Malcome/MalcomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MalcomeApp.swift
+//  Malcome
+//
+//  Created by Mark Friedlander on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct MalcomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
